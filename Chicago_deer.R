bc5ba2a6fc2d54f360da493b75ee500ce5ff3665
YunyiShen <yshen99@outlook.com>
2018-11-22 09:38:58 -0600

    Fail to detect DD in the data

--- Chicago_deer.R
+++ Chicago_deer.R
@@ -17,10 +17,10 @@ Harv.data = read.csv("./data/Culling.csv")
 Harv.data = Harv.data[,-1]
 mean.b = (584 * Harv.data[,1]/sum(Harv.data[,1]))/0.7
 
-Chicago_RES = HDDLislie.sampler( n.iter = 5000, burn.in = 300, mean.f = as.matrix( mean.f)
-                                   ,al.f = 1, be.f = .001, al.s = 1, be.s = .5
+Chicago_RES = HDDLislie.sampler( n.iter = 15000, burn.in = 500, mean.f = as.matrix( mean.f)
+                                   ,al.f = 1, be.f = .001, al.s = 1, be.s = 1
                                    , al.K0 = 1, be.K0 = .001, al.n = 1
-                                   , be.n = .001, al.H = 1, be.H = .01
+                                   , be.n = .001, al.H = 1, be.H = .001
                                    , mean.s = as.matrix(mean.s), mean.b= as.matrix(mean.b),mean.K0 = matrix(700,1,1)
                                    , mean.H = matrix(0.7,nage,1), Harv.data = as.matrix(Harv.data+1e-4 * (Harv.data==0))
                                    , prop.vars = prop.vars, estFer = T,nage = nage)
@@ -29,8 +29,8 @@ mean.surv = apply(Chicago_RES$surv.prop.mcmc,2,mean)
 mean.ferc = apply(Chicago_RES$fert.rate.mcmc,2,mean)
 
 mean.harv = apply(Chicago_RES$lx.mcmc,2,mean)
-mean.harv.matrix = matrix(mean.harv,nrow = nage,ncol = period-1)
-mean.harv.matrix = cbind(mean.harv.matrix,Harv.data[,14])
+mean.harv.matrix = matrix(mean.harv,nrow = nage,ncol = period)
+#mean.harv.matrix = cbind(mean.harv.matrix)
 mean.total.harv = apply(mean.harv.matrix,2,sum)
 
 mean.age.surv = list()
@@ -64,21 +64,14 @@ lapply(DDferc, summary)
 
 
 #mean.harv.2 = mean.harv[(1:(nage*period))%%nage==2]
-plot(mean.total.harv,mean.age.surv[[8]])
+plot(mean.total.harv,mean.age.surv[[7]])
 
-data.age.2 = data.frame(mean.total.harv,mean.surv.1)
 
-for(i in 1:8){
-  formulea = as.formula( paste0("mean.surv.",i,"~mean.total.harv"))
-  DDsurv[[i]] = lm(formulea,data=data.frame(mean.total.harv))
-  
-}
-summaries = lapply(DDsurv,summary)
 
 
 require(ggplot2)
 
-ggplot(data.age.2,aes(x=mean.total.harv,y=mean.surv.2) )+
+ggplot(DDsurv[[8]]$model,aes(x=mean.total.harv,y=mean.age.surv) )+
   geom_point()+
   geom_line()+
   stat_smooth(method = "lm")
