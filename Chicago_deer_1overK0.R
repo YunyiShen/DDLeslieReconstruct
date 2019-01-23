7644d1ed33fc72cb2e599a0c27663f5614755d2d
YunyiShen <yshen99@outlook.com>
2019-01-22 22:38:59 -0600

    Update Chicago_deer_1overK0.R

--- Chicago_deer_1overK0.R
+++ Chicago_deer_1overK0.R
@@ -17,13 +17,20 @@ Harv.data = read.csv("./data/Culling.csv")
 Harv.data = Harv.data[,-1]
 mean.b = (584 * Harv.data[,1]/sum(Harv.data[,1]))/0.7
 
-Chicago_RES = HDDLislie.sampler( n.iter = 5000, burn.in = 300, mean.f = as.matrix( mean.f)
-                                   ,al.f = 1, be.f = .01, al.s = 1, be.s = 1
-                                   , al.K0 = 1, be.K0 = .1, al.n = 1
-                                   , be.n = .01, al.H = 1, be.H = .01
+Chicago_RES = HDDLislie.sampler( n.iter = 50000, burn.in = 300, mean.f = as.matrix( mean.f)
+                                   ,al.f = 1, be.f = .001, al.s = 1, be.s = .01
+                                   , al.aK0 = 1, be.aK0 = .1, al.n = 1
+                                   , be.n = .01, al.H = 1, be.H = .1
                                    , mean.s = as.matrix(mean.s), mean.b= as.matrix(mean.b),mean.aK0 = matrix(0,1,1)
                                    , mean.H = matrix(0.7,nage,1), Harv.data = as.matrix(Harv.data+1e-4 * (Harv.data==0))
-                                   , prop.vars = prop.vars, estFer = T,nage = nage)
+                                   , prop.vars = prop.vars, estFer = T,nage = nage,homo = T)
 
 
-hist(SIMULATION_RESDD$invK0.mcmc)
+hist(Chicago_RES$invK0.mcmc)
+
+mean.harv = apply(Chicago_RES$lx.mcmc,2,mean)
+mean.harv.matrix = matrix(mean.harv,nrow = nage,ncol = period)
+mean.total.harv = apply(mean.harv.matrix,2,sum)
+plot(mean.total.harv)
+
+mean.ferc = apply(Chicago_RES$fert.rate.mcmc,2,mean)
